ce70f342879de8a1c042b8e543975123982f1339
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-15 14:42:15 +1200

    Filter out array based on any size arg

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  for (const args of remove) {
+    array = array.filter(a => a !== args);
+  }
+  return array;
 };
 
 // Do not edit below this line
