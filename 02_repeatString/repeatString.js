2f7b02f6af958ede0d291bc3d23232489a43c5db
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-15 14:02:46 +1200

    Repeat string for any amount

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeat) {
+  let out = "";
+  if (repeat < 0) return "ERROR";
+  for (let i = 0; i < repeat; i++) {
+    out += text;
+  }
+  return out;
 };
 
 // Do not edit below this line
