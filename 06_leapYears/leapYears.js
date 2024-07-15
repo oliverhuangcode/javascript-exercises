0939c87f15759001d79d513176bdba0059868eb5
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-15 15:11:50 +1200

    Check leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // Check divisible by 4 and if divisible by 100 must be divisible by 400 to be true
+  
+  return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? true : false;
 };
 
 // Do not edit below this line
