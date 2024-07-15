5735af6659bbe18b218478e580d2b96f528bc14b
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-15 15:00:39 +1200

    Sum all integers between given values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  // Check if valid inputs
+  if (typeof start !== "number" || typeof end !== "number" || start < 0 || end < 0) return "ERROR";
 
+  // Swap if start is greater
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
